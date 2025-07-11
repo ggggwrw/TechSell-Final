02e1ed5f07e939769ca297245142674d0740fe76
Malbete <56492693+ggggwrw@users.noreply.github.com>
2025-07-11 00:55:04 -0300

    Update productos-manuales.js
    Puse mal la ruta en algunos productos, esto debería solucionar el error

--- productos-manuales.js
+++ productos-manuales.js
@@ -27,84 +27,85 @@ window.productosManual = [
     id: 105,
     title: "PC Gamer Ryzen 7 / RTX 4070",
     price: 1499.99,
-    image: "productos/pcgamer.webp"
+    image: "Productos/pcgamer.webp"
   },
   {
     id: 106,
     title: "Laptop Dell XPS 13 OLED",
     price: 1199.99,
-    image: "productos/lapoled.webp"
+    image: "Productos/lapoled.webp"
   },
   {
     id: 107,
     title: "Disco SSD Samsung 980 Pro 1TB",
     price: 129.99,
-    image: "productos/dss.jpg"
+    image: "Productos/dss.jpg"
   },
   {
     id: 108,
     title: "Placa Madre ASUS ROG Strix X670E",
     price: 379.99,
-    image: "productos/x1-143-jpg.webp"
+    image: "Productos/x1-143-jpg.webp"
   },
   {
     id: 109,
     title: "Gabinete NZXT H510",
     price: 89.99,
-    image: "productos/gabine.webp"
+    image: "Productos/gabine.webp"
   },
   {
     id: 110,
     title: "Fuente Corsair RM850x 850W",
     price: 129.99,
-    image: "productos/corsair.webp"
+    image: "Productos/corsair.webp"
   },
   {
     id: 111,
     title: "Teclado Mecánico Logitech G Pro",
     price: 129.99,
-    image: "productos/tecla.jpg"
+    image: "Productos/tecla.jpg"
   },
   {
     id: 112,
     title: "Mouse Logitech G502 HERO",
     price: 49.99,
-    image: "productos/mouse.jpg"
+    image: "Productos/mouse.jpg"
   },
   {
     id: 113,
     title: "Auriculares Razer BlackShark V2",
     price: 99.99,
-    image: "productos/auris.jfif"
+    image: "Productos/auris.jfif"
   },
   {
     id: 114,
     title: "Webcam Logitech StreamCam 1080p",
     price: 149.99,
-    image: "productos/web.jfif"
+    image: "Productos/web.jfif"
   },
   {
     id: 115,
     title: "Nubia focus 4G",
     price: 170,
-    image: "productos/prod 3.webp"
+    image: "Productos/prod 3.webp"
   },
   {
     id: 116,
     title: "Motorola E15",
     price: 300,
-    image: "productos/prod 2.webp"
+    image: "Productos/prod 2.webp"
   },
   {
     id: 117,
     title: "Samsung Galaxy A15",
     price: 250,
-    image: "productos/prod 1.webp"
+    image: "Productos/prod 1.webp"
   },
 
 
 
-
+    // espacio para sumar productos de manera manual
 
 
 ];
+
